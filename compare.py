33b7e0628d11109ca5a9e1798643b46e08fa1421
Matthew Heston <matthewheston@gmail.com>
2016-02-23 14:44:08 -0600

    First commit.

---
+++ compare.py
@@ -0,0 +1,88 @@
+import urllib2
+import time
+import re
+from bs4 import BeautifulSoup
+from fuzzywuzzy import process, fuzz
+from jinja2 import Template
+import codecs
+import random
+
+USER_AGENTS = ["Mozilla/5.0"]
+
+def get_candidate_soup(candidate):
+    if candidate == "hillary":
+        hillary_issues_url = "https://www.hillaryclinton.com/issues/"
+        hillary_html = urllib2.urlopen(urllib2.Request(hillary_issues_url, headers={ 'User-Agent': random.choice(USER_AGENTS) })).read()
+        hillary_soup = BeautifulSoup(hillary_html, "html.parser")
+        return hillary_soup
+    if candidate == "bernie":
+        bernie_issues_url = "https://berniesanders.com/issues/"
+        # old Bernie takes some finagling
+        bernie_html = urllib2.urlopen(urllib2.Request(bernie_issues_url, headers={ 'User-Agent': random.choice(USER_AGENTS) })).read()
+        bernie_soup = BeautifulSoup(bernie_html, "html.parser")
+        return bernie_soup
+    # if we reached this point there's an issue
+    raise ValueError("candidate not supported")
+
+def get_issues(hillary_soup, bernie_soup):
+    # I know this is ugly, but the issues don't belong to any specific class, and
+    # it seems the easiest way to find them is by their font size.
+    hillary_issues_html = hillary_soup.find_all("p", {"style": re.compile(r".+1\.5em.+") })
+    # Nice use of header tags, Bern. Other candidates could learn a lot from you.
+    bernie_issues_html = bernie_soup.select("h3 > a")
+
+    hillary_issues = [tag.text for tag in hillary_issues_html]
+    bernie_issues = [tag.text for tag in bernie_issues_html]
+
+    return hillary_issues, bernie_issues
+
+def match_issues(hillary_issues, bernie_issues):
+    matched_issues = []
+    for hissue in hillary_issues:
+        matching_bern_issue = process.extractOne(hissue, bernie_issues, scorer=fuzz.token_set_ratio, score_cutoff = 65)
+        if matching_bern_issue:
+            matched_issues.append((hissue, matching_bern_issue[0]))
+    return matched_issues
+
+def get_text_for_issue(hillary_soup, bernie_soup, issue_tuple):
+    hillary_issue = issue_tuple[0]
+    bernie_issue = issue_tuple[1]
+    hillary_text = get_text_for_hillary_issue(hillary_soup, hillary_issue)
+    bernie_text = get_text_for_bernie_issue(bernie_soup, bernie_issue)
+    return hillary_text, bernie_text
+
+def get_text_for_hillary_issue(hillary_soup, issue):
+    issue_tag = hillary_soup.find("p", text=issue)
+    link_tag = issue_tag.find_next("a", text="Read more")
+    bullet_points = extract_bullet_points_from_url(link_tag["href"])
+    return bullet_points
+
+def get_text_for_bernie_issue(bernie_soup, issue):
+    link_tag = bernie_soup.find("a", text=issue)
+    bullet_points = extract_bullet_points_from_url(link_tag["href"])
+    return bullet_points
+
+def extract_bullet_points_from_url(url):
+    soup = BeautifulSoup(urllib2.urlopen(urllib2.Request(url, headers={ 'User-Agent': random.choice(USER_AGENTS) })).read(), "html.parser")
+    bullet_points = soup.find_all("li")
+    text_points = [point.text.replace("\n", "") for point in bullet_points]
+    text_points = [t for t in text_points if len(t.split()) > 5 and "Follow" not in t]
+    return text_points
+
+if __name__ == "__main__":
+    hillary_soup = get_candidate_soup("hillary")
+    bernie_soup = get_candidate_soup("bernie")
+    hillary_issues, bernie_issues = get_issues(hillary_soup, bernie_soup)
+    matched_issues = match_issues(hillary_issues, bernie_issues)
+    issues_template = Template(open("issues_we_found.md.template").read())
+    issues_text = issues_template.render(hillary_issues=hillary_issues, bernie_issues=bernie_issues, matched_issues=matched_issues)
+    with codecs.open("issues_we_found.md", "w", "utf-8") as issues_we_found:
+        issues_we_found.write(issues_text)
+    for issue in matched_issues:
+        issue_template = Template(open("issue_comparison.md.template").read())
+        hillary_text, bernie_text = get_text_for_issue(hillary_soup, bernie_soup, issue)
+        hillary_text = " ".join(hillary_text)
+        bernie_text = " ".join(bernie_text)
+        with codecs.open(issue[0].split()[0] + ".md", "w", "utf-8") as specific_issue:
+            specific_issue.write(issue_template.render(hillary_text=hillary_text, bernie_text=bernie_text))
+        time.sleep(15) / Be kind, take time.
---
+++ issues_we_found.md
@@ -0,0 +1,136 @@
+Here are the issues we found for Hillary:
+
+* Learn more about Hillary’s vision for America, and key policies she will fight for as president.
+
+* 
+
+* As Hillary campaigns across the country, she'll be talking about more issues—stay tuned. 
+
+* Alzheimer's disease
+
+* Campaign finance reform
+
+* Campus sexual assault
+
+* Climate change and energy
+
+* College
+
+* Criminal justice reform
+
+* Disability rights
+
+* Early childhood education
+
+* Economy
+
+* Gun violence prevention
+
+* Health care
+
+* Immigration reform
+
+* Infrastructure
+
+* K–12 education
+
+* Labor
+
+* LGBT equality
+
+* National security
+
+* Paid leave
+
+* Racial justice
+
+* Rural communities
+
+* Small business
+
+* Social Security and Medicare
+
+* Substance use disorder and addiction
+
+* Veterans, the armed forces, and their families
+
+* Voting rights
+
+* Wall Street and corporate America
+
+* Women’s rights and opportunity
+
+* Workforce and skills
+
+
+Here are the issues we found for Bernie:
+
+* Income and Wealth Inequality
+
+* It's Time to Make College Tuition Free and Debt Free
+
+* Getting Big Money Out of Politics and Restoring Democracy
+
+* Creating Decent Paying Jobs
+
+* A Living Wage
+
+* Combating Climate Change to Save the Planet
+
+* A Fair and Humane Immigration Policy
+
+* Racial Justice
+
+* Fighting for Women's Rights
+
+* Fighting for LGBT Equality
+
+* Caring for Our Veterans
+
+* Medicare For All
+
+* Fighting for Disability Rights
+
+* Strengthen and Expand Social Security
+
+* Fighting to Lower Prescription Drug Prices
+
+* Improving the Rural Economy
+
+* Reforming Wall Street
+
+* Real Family Values
+
+* War and Peace
+
+* War Should Be the Last Option: Why I Support the Iran Deal
+
+* Making the Wealthy, Wall Street, and Large Corporations Pay their Fair Share
+
+* How Bernie pays for his proposals
+
+
+Here is the best we did at matching issues from one to the other (which we know
+isn't perfect!)
+| Hillary | Bernie |
+| ------  | -----  |
+
+|[Climate.md](Climate change and energy) | {[Climate.md](Combating Climate Change to Save the Planet) |
+
+|[College.md](College) | {[College.md](It's Time to Make College Tuition Free and Debt Free) |
+
+|[Criminal.md](Criminal justice reform) | {[Criminal.md](Racial Justice) |
+
+|[Disability.md](Disability rights) | {[Disability.md](Fighting for Disability Rights) |
+
+|[Economy.md](Economy) | {[Economy.md](Improving the Rural Economy) |
+
+|[Immigration.md](Immigration reform) | {[Immigration.md](A Fair and Humane Immigration Policy) |
+
+|[LGBT.md](LGBT equality) | {[LGBT.md](Fighting for LGBT Equality) |
+
+|[Racial.md](Racial justice) | {[Racial.md](Racial Justice) |
+
+|[Social.md](Social Security and Medicare) | {[Social.md](Strengthen and Expand Social Security) |
+
+|[Wall.md](Wall Street and corporate America) | {[Wall.md](Reforming Wall Street) |
